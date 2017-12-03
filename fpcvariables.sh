2994766f365c28bf6c8e8950b42cba74f54a79b9
maciej-izak <hnb.code@gmail.com>
2017-12-04 00:52:48 +0100

    export variables for fpc

--- fpcvariables.sh
+++ fpcvariables.sh
@@ -7,84 +7,84 @@ set -ev
 
 case $CPUTARGET in
     i386)
-        _OS_TARGET=linux
-        _CPU_TARGET=i386
-        _SUBARCH=
-        _PPCROSS=ppcross386
-        _BINUTILSPREFIX=
-        _OPT=-O3
-        _CROSSOPT=
-        _TEST_OPT=
-        _TEST_FPC="$_PPCROSS"
-        _TEST_SUBARCH=
-        _EMULATOR=
-        HAS_CROSS=true
-        HAS_TESTCHUNK=true
-        TESTS_DIR=i386-linux
+        export _OS_TARGET=linux
+        export _CPU_TARGET=i386
+        export _SUBARCH=
+        export _PPCROSS=ppcross386
+        export _BINUTILSPREFIX=
+        export _OPT=-O3
+        export _CROSSOPT=
+        export _TEST_OPT=
+        export _TEST_FPC="$_PPCROSS"
+        export _TEST_SUBARCH=
+        export _EMULATOR=
+        export HAS_CROSS=true
+        export HAS_TESTCHUNK=true
+        export TESTS_DIR=i386-linux
         ;;
     x86_64)
-        _OS_TARGET=
-        _CPU_TARGET=x86_64
-        _SUBARCH=
-        _PPCROSS=
-        _BINUTILSPREFIX=
-        _OPT=
-        _CROSSOPT=
-        _TEST_OPT=
-        _TEST_FPC=ppcx64
-        _TEST_SUBARCH=
-        _EMULATOR=
-        HAS_CROSS=false
-        HAS_TESTCHUNK=true
-        TESTS_DIR=x86_64-linux
+        export _OS_TARGET=
+        export _CPU_TARGET=x86_64
+        export _SUBARCH=
+        export _PPCROSS=
+        export _BINUTILSPREFIX=
+        export _OPT=
+        export _CROSSOPT=
+        export _TEST_OPT=
+        export _TEST_FPC=ppcx64
+        export _TEST_SUBARCH=
+        export _EMULATOR=
+        export HAS_CROSS=false
+        export HAS_TESTCHUNK=true
+        export TESTS_DIR=x86_64-linux
         ;;
     avr)
-        _OS_TARGET=embedded
-        _CPU_TARGET=avr
-        _SUBARCH=avr5
-        _PPCROSS=ppcrossavr
-        _BINUTILSPREFIX=avr-
-        _OPT=-O3
-        _CROSSOPT=
-        _TEST_OPT="-O4 -al -XPavr- -Wpavrsim"
-        _TEST_FPC="$_PPCROSS"
-        _TEST_SUBARCH="$_SUBARCH"
-        _EMULATOR=$main_dir/run-avr
-        HAS_CROSS=true
-        HAS_TESTCHUNK=true
-        TESTS_DIR=avr-embedded
+        export _OS_TARGET=embedded
+        export _CPU_TARGET=avr
+        export _SUBARCH=avr5
+        export _PPCROSS=ppcrossavr
+        export _BINUTILSPREFIX=avr-
+        export _OPT=-O3
+        export _CROSSOPT=
+        export _TEST_OPT="-O4 -al -XPavr- -Wpavrsim"
+        export _TEST_FPC="$_PPCROSS"
+        export _TEST_SUBARCH="$_SUBARCH"
+        export _EMULATOR=$main_dir/run-avr
+        export HAS_CROSS=true
+        export HAS_TESTCHUNK=true
+        export TESTS_DIR=avr-embedded
         ;;
     i8086)
-        _OS_TARGET=msdos
-        _CPU_TARGET=i8086
-        _SUBARCH=
-        _PPCROSS=ppcross8086
-        _BINUTILSPREFIX=
-        _OPT="-CX -XX -XP -XXs"
-        _CROSSOPT="-WmMedium"
-        _TEST_OPT="-CX -XX -XP -XXs -WmMedium"
-        _TEST_FPC="$_PPCROSS"
-        _TEST_SUBARCH=
-        _EMULATOR=$main_dir/$fpcsrc/tests/utils/dosbox/dosbox_wrapper
-        HAS_CROSS=true
-        HAS_TESTCHUNK=false
-        TESTS_DIR=msdos
+        export _OS_TARGET=msdos
+        export _CPU_TARGET=i8086
+        export _SUBARCH=
+        export _PPCROSS=ppcross8086
+        export _BINUTILSPREFIX=
+        export _OPT="-CX -XX -XP -XXs"
+        export _CROSSOPT="-WmMedium"
+        export _TEST_OPT="-CX -XX -XP -XXs -WmMedium"
+        export _TEST_FPC="$_PPCROSS"
+        export _TEST_SUBARCH=
+        export _EMULATOR=$main_dir/$fpcsrc/tests/utils/dosbox/dosbox_wrapper
+        export HAS_CROSS=true
+        export HAS_TESTCHUNK=false
+        export TESTS_DIR=msdos
         ;;
     m68k)
-        _OS_TARGET=linux
-        _CPU_TARGET=m68k
-        _SUBARCH=
-        _PPCROSS=ppcross68k
-        _BINUTILSPREFIX=m68k-elf-
-        _OPT=
-        _CROSSOPT="-O- -Cpisac"
-        _TEST_OPT="-O- -Cpisac -XPm68k-elf-"
-        _TEST_FPC="$_PPCROSS"
-        _TEST_SUBARCH=
-        _EMULATOR=$main_dir/run-qemu-m68k
-        HAS_CROSS=true
-        HAS_TESTCHUNK=true
-        TESTS_DIR=m68k-linux
+        export _OS_TARGET=linux
+        export _CPU_TARGET=m68k
+        export _SUBARCH=
+        export _PPCROSS=ppcross68k
+        export _BINUTILSPREFIX=m68k-elf-
+        export _OPT=
+        export _CROSSOPT="-O- -Cpisac"
+        export _TEST_OPT="-O- -Cpisac -XPm68k-elf-"
+        export _TEST_FPC="$_PPCROSS"
+        export _TEST_SUBARCH=
+        export _EMULATOR=$main_dir/run-qemu-m68k
+        export HAS_CROSS=true
+        export HAS_TESTCHUNK=true
+        export TESTS_DIR=m68k-linux
         ;;
     *)
         exit 1
